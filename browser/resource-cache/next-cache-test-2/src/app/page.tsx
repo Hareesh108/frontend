ea724ac36b688dee2f24da6485e1c9abdb9c48f0
Hareesh108 <hareeshbhittam@gmail.com>
2025-02-07 15:58:45 +0530

    Working on test 2

---
+++ browser/resource-cache/next-cache-test-2/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Testing Partitioning</h1>
+      <img src="https://www.gstatic.com/webp/gallery/2.jpg" width="300" />
+    </div>
+  );
+}
