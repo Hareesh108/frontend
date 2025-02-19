800397a98696cfab953dd53f46e6f40fe1e220f7
Hareesh108 <hareeshbhittam@gmail.com>
2025-02-19 11:59:16 +0530

    Working on error

---
+++ next-js/nextjs-api-error-handle-mechanism/src/app/(main)/todos/[id]/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import BackButton from "@/components/back-list/back-button";
+
+const Error = () => {
+  return (
+    <div className="text-center h-lvh flex flex-col justify-center items-center">
+      <BackButton />
+    </div>
+  );
+};
+
+export default Error;
---
+++ next-js/nextjs-api-error-handle-mechanism/src/app/(main)/todos/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { Root } from "@/types/product";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  const [todos, setTodos] = useState<Root>();
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/todos?_limit=3"
+        );
+        setTodos(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          setError(error.message);
+        } else {
+          setError("An unexpected error occurred");
+        }
+      }
+    };
+
+    fetchTodos();
+  }, []);
+
+  return (
+    <div className="text-center h-lvh flex flex-col justify-center items-center">
+      <h1>TODOS</h1>
+      {error && <p>Error: {error}</p>}
+      <ul>
+        {todos?.map((todo) => (
+          <div key={todo?.id}>
+            <li key={todo?.id}>{todo?.title}</li>
+            <button
+              // onClick={() => router.push(`/todos/${todo.id}`)}
+              onClick={() => router.push(`/todos/hed`)}
+              className="border-4 p-2 rounded-lg"
+            >
+              Go to Todo
+            </button>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+}
