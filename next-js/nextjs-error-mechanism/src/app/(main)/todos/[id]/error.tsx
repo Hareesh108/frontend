6dc81a278f63cf5906f1cab2dce4816dcfc139c2
Hareesh108 <hareeshbhittam@gmail.com>
2025-02-20 21:44:45 +0530

    checked some error message

--- next-js/nextjs-error-mechanism/src/app/(main)/todos/[id]/error.tsx
+++ next-js/nextjs-error-mechanism/src/app/(main)/todos/[id]/error.tsx
@@ -5,6 +5,7 @@ import BackButton from "@/components/back-list/back-button";
 const Error = () => {
   return (
     <div className="text-center h-lvh flex flex-col justify-center items-center">
+      <h1>oops! something went wrong!</h1>
       <BackButton />
     </div>
   );
