ce5f50237a40c8c3d42c819fed60eb13cd9c9800
hareesh <hareeshbhittam@gmail.com>
2025-06-14 22:55:06 +0530

    HOC test

--- react/concepts/src/high-order-component/counter-example/AddCounter.tsx
+++ react/concepts/src/high-order-component/counter-example/AddCounter.tsx
@@ -3,19 +3,13 @@ import React from "react";
 interface AddCounterProps {
   count: number;
   increment: () => void;
-  decrement: () => void;
 }
 
-const AddCounter: React.FC<AddCounterProps> = ({
-  count,
-  increment,
-  decrement,
-}) => {
+const AddCounter: React.FC<AddCounterProps> = ({ count, increment }) => {
   return (
     <div>
       <h2>Count: {count}</h2>
       <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
     </div>
   );
 };
