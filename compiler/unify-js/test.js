5352ca780a6fe8882c516eed05fe3fee719c8a45
hareesh <hareeshbhittam@gmail.com>
2025-08-10 16:53:12 +0530

    Testing compliler

--- compiler/unify-js/test.js
+++ compiler/unify-js/test.js
@@ -1,6 +1,7 @@
 const double = (x) => { return x + x; };
 const num = 5;
-const str = "hello";
+// const str = "hello";
+const str = 6;
 
 const doubledNum = double(num);
 const mixed = double(num) + double(str);
