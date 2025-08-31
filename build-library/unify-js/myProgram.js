7747de46c403618106a65c4217152827ab632179
hareesh <hareeshbhittam@gmail.com>
2025-08-31 21:08:20 +0530

    Tested inify js program working

--- build-library/unify-js/myProgram.js
+++ build-library/unify-js/myProgram.js
@@ -1,7 +1,7 @@
 // myProgram.js
 const double = (x) => { return x + x; };
 const num = 5;
-const str = 3;
+const str = "Hello";
 
 const doubledNum = double(num);
 const mixed = double(num) + double(str); // ❌ Type error
