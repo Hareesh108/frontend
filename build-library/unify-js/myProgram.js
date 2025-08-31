cc06e82d2f554a7412c691eeb851b947eef6b708
hareesh <hareeshbhittam@gmail.com>
2025-08-31 21:03:26 +0530

    Tested inify js program

---
+++ build-library/unify-js/myProgram.js
@@ -0,0 +1,7 @@
+// myProgram.js
+const double = (x) => { return x + x; };
+const num = 5;
+const str = "hello";
+
+const doubledNum = double(num);
+const mixed = double(num) + double(str); // ❌ Type error
