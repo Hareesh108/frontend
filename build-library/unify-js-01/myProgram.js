accf123253415e82af392403d7efbf0b26845b85
hareesh <hareeshbhittam@gmail.com>
2025-09-01 16:00:23 +0530

    Tested unify js

--- build-library/unify-js-01/myProgram.js
+++
@@ -1,7 +0,0 @@
-// myProgram.js
-const double = (x) => { return x + x; };
-const num = 5;
-const str = 10;
-
-const doubledNum = double(num);
-const mixed = double(num) + double(str); // ❌ Type error
