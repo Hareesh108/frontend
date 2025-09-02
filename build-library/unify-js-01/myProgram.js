c4212cb81b63fb76317372e75628c6d50a8180ad
hareesh <hareeshbhittam@gmail.com>
2025-09-02 17:06:15 +0530

    Checked string type

--- build-library/unify-js-01/myProgram.js
+++ build-library/unify-js-01/myProgram.js
@@ -1,7 +1,7 @@
 // myProgram.js
 const double = (x) => { return x + x; };
-const num = 'Hey';
-const str = "Hello";
+const num = 30;
+const str = 20;
 
 const doubledNum = double(num);
 const mixed = double(num) + double(str); 
