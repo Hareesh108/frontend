a6ad0fbc3b20788b1533f8bfbc5a1476a31201b1
Hareesh108 <hareeshbhittam@gmail.com>
2025-10-11 23:19:28 +0530

    Minor change

--- react/react-19-2-playground/src/UseEffectEventDemo.jsx
+++ react/react-19-2-playground/src/UseEffectEventDemo.jsx
@@ -11,12 +11,21 @@ export default function UseEffectEventDemo() {
   const [theme, setTheme] = useState("light");
   const [msg, setMsg] = useState("");
 
+  console.log("msg:", msg);
+
   const onConnected = useEffectEvent(() => {
     setMsg(`Connected! theme=${theme}`);
   });
 
+  let count = 0
+
   useEffect(() => {
-    const conn = fakeConnection(roomId, ev => ev === "connected" && onConnected());
+    const conn = fakeConnection(
+      roomId,
+      (ev) => ev === "connected" && onConnected()
+    );
+    console.log("count:",++count);
+    
     return () => conn.disconnect();
   }, [roomId]);
 
@@ -24,8 +33,10 @@ export default function UseEffectEventDemo() {
     <div>
       <div>Room: {roomId}</div>
       <div>Theme: {theme}</div>
-      <button onClick={() => setRoomId(r => r + 1)}>Change room</button>
-      <button onClick={() => setTheme(t => (t === "light" ? "dark" : "light"))}>
+      <button onClick={() => setRoomId((r) => r + 1)}>Change room</button>
+      <button
+        onClick={() => setTheme((t) => (t === "light" ? "dark" : "light"))}
+      >
         Toggle theme
       </button>
       <div>{msg}</div>
