2cadb779d4870237726e0e46d47b858419173009
Hareesh108 <hareeshbhittam@gmail.com>
2025-12-04 12:14:27 +0530

    Test typescript

--- javascript/test.js
+++ javascript/test.js
@@ -1,5 +1,13 @@
-let str = "Hareesh"
+let str = "Hareesh";
 
-let hey =  str.split("").reverse("").join("")
+console.log(str.substring(2, 4));
+
+let hey = str.split("").reverse("").join("");
 
 console.log(hey);
+
+let arr = [1, 2, 3, 33333, 3, 21, 312];
+
+console.log(arr.sort((a, b) => a - b));
+
+console.log(arr.reduce((pre, cur) => pre + cur, 0));
