453ad5ad58f811f5cf124f9c8bb00da6f0bf125a
Hareesh108 <hareeshbhittam@gmail.com>
2026-02-08 23:29:05 +0530

    Added scope

---
+++ javascript/deep-javascript/coercion/boxing/boxing copy.js
@@ -0,0 +1,13 @@
+
+if(inputElement.value.length > 50){
+
+}
+
+"hello".length            // 5
+"hello".toUpperCase()     // "HELLO"
+
+(42).toFixed(2)           // "42.00"
+(3.14159).toPrecision(2) // "3.1"
+
+true.toString()           // "true"
+false.valueOf()           // false
