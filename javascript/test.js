b96631b6a0f75c5ec2f77a612255b29f8a1be3cd
Hareesh108 <hareeshbhittam@gmail.com>
2026-02-16 23:23:54 +0530

    stri t mode

--- javascript/test.js
+++ javascript/test.js
@@ -1,8 +1,8 @@
-let str = "Hareesh";
+let str = 'Hareesh';
 
 console.log(str.substring(2, 4));
 
-let hey = str.split("").reverse("").join("");
+let hey = str.split('').reverse('').join('');
 
 console.log(hey);
 
@@ -11,3 +11,5 @@ let arr = [1, 2, 3, 33333, 3, 21, 312];
 console.log(arr.sort((a, b) => a - b));
 
 console.log(arr.reduce((pre, cur) => pre + cur, 0));
+
+console.log('hello');
