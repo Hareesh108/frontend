1c12d25a0de7323a080c97c75aa567a0c6d8a4c3
Hareesh108 <hareeshbhittam@gmail.com>
2026-02-18 23:17:35 +0530

    Added two three equlity check

--- test.js
+++ test.js
@@ -1,2 +1,4 @@
-const instant = Temporal.Instant.from("2023-10-01T12:00:00Z");
+const instant = Temporal.Instant.from('2023-10-01T12:00:00Z');
 console.log(instant.toString()); // "2023-10-01T12:00:00Z"
+
+console.log('tests');
